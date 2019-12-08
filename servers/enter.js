53c7a258b2d0078702dd6dd08e420afee0782c06
congratulatio <45519482+congratulatio@users.noreply.github.com>
2019-12-08 20:58:54 +0000

    Changes to enter and clienterror servers
    - Logging improvements and refactoring
    - Now uses express for http server

--- servers/enter.js
+++ servers/enter.js
@@ -1,33 +1,74 @@
-const http = require('http');
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
+const app = express()
+app.disable('x-powered-by');
+
+/*
+ *  Internal endpoint, proxied via nginx
+ */
 
 const hostname = 'localhost';
 const port = 3333;
 
-var fs = require('fs')
-fs.appendFile('enter.log', '---- ' + (new Date().toISOString()) + ' ' + '-'.repeat(50) + '\n', function (err) {
-  if (err) { console.log("error writing to log"); }
+/*
+ *  Logging helper
+ */
+
+const logfile = path.resolve(__dirname, '../logs', path.basename(__filename, '.js') + '.log');
+
+var errstr = function(err) {
+  let obj = {};
+  Object.getOwnPropertyNames(err).forEach(name => obj[name] = err[name]);
+  return JSON.stringify(obj);
+}
+
+var log = function() {
+  let parts = [...arguments].map(part => part instanceof Error ? errstr(part) : part);
+  let msg = (new Date().toISOString()) + ' | ' + parts.join(' | ') + '\n';
+  fs.appendFileSync(logfile, msg, e => {
+    console.error(`error writing to log:\n  ${errstr(e)}\n  ${msg}`);
+  });
+}
+
+/*
+ *  POST https://data.airmash.online/enter
+ */
+
+app.post('/', (req, res) => {
+  let ip = req.headers['x-real-ip'] || req.connection.remoteAddress;
+
+  let body = '';
+  req.on('data', (chunk) => { body += chunk; });
+
+  req.on('end', () => {
+    log(ip, JSON.stringify(req.headers), JSON.stringify(body));
+    res.status(200).type('json').end('{"result":1}');
+  });
 });
 
-const server = http.createServer((req, res) => {
-  if (req.method === 'POST') {
-    let body = '';
-    req.on('data', chunk => {
-        body += chunk.toString();
-    });
-    req.on('end', () => {
-        var fs = require('fs')
-        var logstring = (new Date().toISOString()) + ' | ' + req.headers['x-real-ip'] + ' | ' + JSON.stringify(req.headers) + ' | ' + body;
-        console.log(logstring);
-        fs.appendFile('enter.log', logstring + '\n', function (err) {
-          if (err) { console.log("error writing to log"); }
-        });
-    });
-  }
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'application/json; charset=utf-8');
-  res.end('{"result":1}');
+/*
+ *  Default route
+ */
+
+app.use(function (req, res) {
+  res.status(204).end();
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+/*
+ *  Error handling
+ */
+
+app.use(function(err, req, res, next) {
+  log(req.reqid, 'error', e);
+  res.status(500).end();
+});
+
+/*
+ *  Start application
+ */
+
+app.listen(port, hostname, () => {
+  log('start', `server running at http://${hostname}:${port}/`);
 });
